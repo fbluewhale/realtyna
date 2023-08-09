b9e04b562a0e067f9d064a78aacbb014a86c9ed6
farnam <bluewhale.f@gmail.com>
2023-08-09 15:03:27 +0330

    create serializer for booking

--- rooms/urls.py
+++ rooms/urls.py
@@ -1,7 +1,7 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 from .views import RoomsViewSet
-from .views import    RoomsViewSet
+from .views import    AvailableRoomsViewSet
 
 router = DefaultRouter()
 router.register(r"room", RoomsViewSet)
@@ -9,4 +9,6 @@ router.register(r"room", RoomsViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
+    path("available_room/", AvailableRoomsViewSet.as_view()),
+
 ]
