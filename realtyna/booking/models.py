0ed9836a2a2eb91a331b13a96368e8eed2b00439
farnam <bluewhale.f@gmail.com>
2023-08-16 23:44:01 +0330

    remove date and set range date for booking

--- realtyna/booking/models.py
+++ realtyna/booking/models.py
@@ -4,11 +4,14 @@ from utils.models import BaseModel
 
 
 class ReservedRoom(BaseModel):
-    date = models.DateField()
+    # date = models.DateField()
     room = models.ForeignKey(
         Room, related_name="reserved_room", on_delete=models.PROTECT
     )
     owner = models.CharField(max_length=100)
+    booking_date = models.DateField(auto_now_add=True)
+    checking_date = models.DateField(blank=True, null=True)
+    checkout_date = models.DateField(null=True, blank=True)
 
     def __str__(self) -> str:
-        return f"{self.room.title}-{self.room.number}-{self.date}--{self.owner}"
+        return f"{self.room.title}-{self.room.number}-{self.checking_date}-{self.checkout_date }-{self.owner}"
