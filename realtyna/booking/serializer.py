30c02e735c0f245cc5b6363ad7105c2c24d1f7a0
farnam <bluewhale.f@gmail.com>
2023-08-16 23:48:38 +0330

    change fixed date to date_range in serializers

--- realtyna/booking/serializer.py
+++ realtyna/booking/serializer.py
@@ -8,20 +8,26 @@ from utils.exception import ValidationException
 class ReservedRoomSerializer(serializers.ModelSerializer):
     class Meta:
         model = ReservedRoom
-        exclude = ("id", "created_at", "updated_at")
+        exclude = ("id", "created_at", "updated_at","booking_date")
 
 
 class AvailableRoomSerializer(serializers.Serializer):
-    date = serializers.DateTimeField(required=True, input_formats=["%Y-%m-%d"])
+    checking_date = serializers.DateTimeField(required=True, input_formats=["%Y-%m-%d"])
+    checkout_date = serializers.DateTimeField(required=True, input_formats=["%Y-%m-%d"])
 
     def validate_date(self, value):
         if value >= datetime.datetime.now().replace(tzinfo=pytz.UTC):
             raise ValidationException("")
         return value
 
+    def validate(self, data):
+        if data.get("checkout_date") < data.get("checking_date"):
+            raise ValidationException("checkout date most be grater than checking date")
+        return data
 
 class CheckRoomIsAvailableSerializer(serializers.Serializer):
-    date = serializers.DateTimeField(input_formats=["%Y-%m-%d"])
+    checking_date = serializers.DateTimeField(required=True, input_formats=["%Y-%m-%d"])
+    checkout_date = serializers.DateTimeField(required=True, input_formats=["%Y-%m-%d"])
     room = serializers.UUIDField()
 
 
@@ -38,10 +44,10 @@ class BookedRoomReportQuerySerializer(serializers.Serializer):
     start_date = serializers.DateTimeField(input_formats=["%Y-%m-%d"])
     end_date = serializers.DateTimeField(input_formats=["%Y-%m-%d"])
 
-    def validate_start_date(self, value):
-        if value >= datetime.datetime.now().replace(tzinfo=pytz.UTC):
-            raise ValidationException("")
-        return value
+    # def validate_start_date(self, value):
+    #     if value >= datetime.datetime.now().replace(tzinfo=pytz.UTC):
+    #         raise ValidationException("")
+    #     return value
 
     def validate(self, data):
         if data.get("end_date") < data.get("start_date"):
